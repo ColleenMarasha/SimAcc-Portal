bda458875b09854d9ec05fa1d58ce8e104d8f8e2
Colleen Marasha <colleenmarasha01@gmail.com>
2025-05-13 12:57:43 +0200

    Primary Navigations around the App

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import './index.css'; // Keep your global styles
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import BrowserRouter
+import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* Wrap App with BrowserRouter */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
---
+++ src/start.js
@@ -0,0 +1,17 @@
+import logo from './assets/logo.png';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <div>SimAcc Portal Frontend</div>
+     
+      </header>
+    </div>
+  );
+}
+
+export default App;
